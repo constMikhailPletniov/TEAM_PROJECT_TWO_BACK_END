26e1449ef8f86c1e7e53aca39d1ccab057c148fc
constMikhailPletniov <mihailpl1995@gmail.com>
2022-01-06 02:29:54 +0200

    add genres to request? but not fix

--- server/dataBase/repositories/movies.repositories.js
+++ server/dataBase/repositories/movies.repositories.js
@@ -3,20 +3,21 @@ const client = require('../dataBases');
 
 let count = 1;
 
-const setMovies = async ({ id }) => {
+const setMovies = async ({ id }, genresItem) => {
     try {
 
-        const { data: { adult, backdrop_path, budget, homepage, imdb_id, original_language, original_title, overview, popularity, poster_path, release_date, revenue, runtime } } = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=483f32e50b323d6e44691437daeb45e7`);
+        const { data: { adult, backdrop_path, budget, homepage, imdb_id, original_language, original_title, overview, popularity, poster_path, release_date, revenue, runtime, genres } } = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=483f32e50b323d6e44691437daeb45e7`);
+        console.log(genres);
+        //  const result = await client.query(`INSERT INTO movies(adult, backdrop_path, budget, homepage, imdb_id, original_language, original_title, overview, popularity, poster_path, release_date, revenue, runtime)
+        //  VALUES(${adult},'${backdrop_path}',${budget},'${homepage}','${imdb_id}','${original_language}','${original_title}','${overview}',${popularity},'${poster_path}','${release_date}',${revenue},${runtime})`);
 
-        const result = await client.query(`INSERT INTO movies(adult, backdrop_path, budget, homepage, imdb_id, original_language, original_title, overview, popularity, poster_path, release_date, revenue, runtime)
-          VALUES(${adult},'${backdrop_path}',${budget},'${homepage}','${imdb_id}','${original_language}','${original_title}','${overview}',${popularity},'${poster_path}','${release_date}',${revenue},${runtime})`);
-
-        return { data: result };
+        //  return { data: result };
     } catch (err) {
         console.error('setMovies: ', err);
         return { error: err };
     }
 };
+
 
 const getIdMovies = async () => {
     try {
@@ -26,7 +27,7 @@ const getIdMovies = async () => {
         }
         count++;
 
-        if (count > 10) {
+        if (count > 12) {
             return;
         }
         getIdMovies();
@@ -36,6 +37,20 @@ const getIdMovies = async () => {
     }
 };
 
+// const getGenres = async () => {
+//     try {
+//         const { genres } = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=483f32e50b323d6e44691437daeb45e7`);
+
+//         for (const item of genres) {
+//             await setMovies(null, item);
+//         }
+//     } catch (err) {
+//         console.error('getGenres: ', err);
+//         return { error: err };
+//     }
+
+// };
+// getGenres();
 module.exports = {
     getIdMovies,
     setMovies
