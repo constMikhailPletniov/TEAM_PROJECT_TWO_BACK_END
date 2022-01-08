a2cdf7a80145a272e5c8c4d132e66457f23695b4
shapikso <79461623+shapikso@users.noreply.github.com>
2022-01-08 14:15:15 +0200

    bug fixed

--- server/controllers/movies.controllers.js
+++ server/controllers/movies.controllers.js
@@ -4,10 +4,8 @@ const { MOVIES_REPOSITORIES } = require('../dataBase/repositories');
 const setMoviesControll = async ({ user_role }) => {
     try {
         if (!user_role === 'admin') return { error: "Invalid admin" };
-        const data = await MOVIES_REPOSITORIES.getIdMovies();
-        console.log(data);
-        if (!data) return { error: "From api No data" };
-        return { data: data };
+        await MOVIES_REPOSITORIES.getIdMovies();
+        return { data: 'Movies was set' };
     } catch (err) {
         console.error('setMoviesControll: ', err);
         return { error: err };
@@ -18,7 +16,6 @@ const getMovies = async () => {
     try {
         const { data } = await MOVIES_REPOSITORIES.getMovies();
         if (!data[0]) return { data: 'Not found', status: STATUS_CODE.NOT_FOUND };
-
         return { data: data };
     } catch (err) {
         console.error('getMovies: ', err);
